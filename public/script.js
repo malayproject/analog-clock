3113724347f54d2936ac2814ccac5aa0348b8d27
malayproject <malay.s1006@gmail.com>
2023-11-22 20:00:19 +0530

    nearly built thye feature

--- public/script.js
+++ public/script.js
@@ -1,6 +1,19 @@
 const secondHandEl = document.querySelector(".secondHand");
 const minuteHandEl = document.querySelector(".minuteHand");
 const hourHandEl = document.querySelector(".hourHand");
+const sliderEl = document.querySelector(".slider");
+const sliderKnobEl = document.querySelector(".sliderKnob");
+const appContainerEl = document.querySelector(".appContainer");
+const clockFrameImgEl = document.querySelector("#clockFrameImg");
+
+sliderEl.addEventListener("click", (e) => {
+  sliderKnobEl.classList.toggle("dark");
+  sliderEl.classList.toggle("dark");
+  appContainerEl.classList.toggle("dark");
+  clockFrameImgEl.classList.toggle("dark");
+  minuteHandEl.classList.toggle("dark");
+  hourHandEl.classList.toggle("dark");
+});
 
 function getTime() {
   const currTime = new Date();
@@ -17,3 +30,45 @@ function getTime() {
 }
 
 setTimeout(getTime, 1000);
+
+function getClockFaceSetter() {
+  // let currFaceIndex = 0;
+  let currFaceIndex = Number(localStorage.getItem("preferredClockFace")) || 0;
+  const themeSelectEl = document.querySelector(".themeSelect");
+  themeSelectEl.innerText = clockFaces[currFaceIndex].name;
+  clockFrameImgEl.setAttribute("src", clockFaces[currFaceIndex].imageUrl);
+  return function (next = false) {
+    if (next) {
+      if (currFaceIndex === clockFaces.length - 1) currFaceIndex = 0;
+      else currFaceIndex += 1;
+    } else {
+      if (currFaceIndex === 0) currFaceIndex = clockFaces.length - 1;
+      else currFaceIndex -= 1;
+    }
+    localStorage.setItem("preferredClockFace", currFaceIndex);
+    themeSelectEl.innerText = clockFaces[currFaceIndex].name;
+    clockFrameImgEl.setAttribute("src", clockFaces[currFaceIndex].imageUrl);
+    console.log(clockFrameImgEl);
+  };
+}
+
+let clockFaces;
+
+function init() {
+  fetch("../clockFaceConfig.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      clockFaces = data.clockFaces;
+      console.log(clockFaces);
+      const setClockFace = getClockFaceSetter();
+      document
+        .querySelector("#leftArrow")
+        .addEventListener("click", setClockFace);
+      document
+        .querySelector("#rightArrow")
+        .addEventListener("click", () => setClockFace(true));
+    });
+}
+
+init();
